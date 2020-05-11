2611ff0015f8f9bc036139763a0c54b80d67a7d6
Md Abu Ahsan Basir <maab.career@gmail.com>
2020-05-11 17:21:51 +0600

    Add install command

---
+++ src/Install.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Codexshaper\WooCommerce\PHP;
+
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
+
+class Install
+{
+    public static function postUpdate(Event $event)
+    {
+        $composer = $event->getComposer();
+        // do stuff
+    }
+
+    public static function postAutoloadDump(Event $event)
+    {
+        $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
+        require $vendorDir . '/autoload.php';
+
+        mkdir($vendorDir.'/basir', 0700);
+        mkdir($vendorDir.'/../test', 0700);
+
+        // some_function_from_an_autoloaded_file();
+    }
+
+    public static function postPackageInstall(PackageEvent $event)
+    {
+        $installedPackage = $event->getOperation()->getPackage();
+        // do stuff
+
+        var_dump($installedPackage);
+    }
+
+    public static function warmCache(Event $event)
+    {
+        // make cache toasty
+    }
+}
